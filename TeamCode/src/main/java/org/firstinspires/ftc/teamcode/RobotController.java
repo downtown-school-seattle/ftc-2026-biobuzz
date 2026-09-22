c19293e25923c4538887bd2fb3f12a5d5a0334b6
Zen <zentrix278@gmail.com>
2026-09-22 15:34:41 -0700

    added host name acquierer for robot name identification

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotController.java
@@ -1,16 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Objects;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 abstract public class RobotController extends LinearOpMode {
+
+    public static final String BernardHost = "NULL";
+    public static final String GladysHost = "NULL";
+
+    public static String getLocalHost() {
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+            return localHost.getHostName();
+        } catch (UnknownHostException e) {
+            return "Unknown Host: " + e.getMessage();
+        }
+    }
+
     /** Enum representing the two physical bots we have. */
     public enum BotIdentity {
         Bernard,
-        Gladys
+        Gladys,
+        Unknown
     }
 
     /** Return which bot we are running on. */
-    abstract BotIdentity getIdentity();
+    public static BotIdentity getIdentity() {
+        if (Objects.equals(getLocalHost(), BernardHost)) {
+            System.out.println("Bernard: " + getLocalHost());
+            return BotIdentity.Bernard;
+        }
+        if (Objects.equals(getLocalHost(), GladysHost)) {
+            System.out.println("Gladys: " + getLocalHost());
+            return BotIdentity.Gladys;
+        }
+        else {
+            System.out.println(getLocalHost());
+            return BotIdentity.Unknown;
+        }
+    }
 
     /** Switch a constant between two values based on what robot we are running this on. */
     protected final <T> T switchOnBot(T bernardValue, T gladysValue) {
